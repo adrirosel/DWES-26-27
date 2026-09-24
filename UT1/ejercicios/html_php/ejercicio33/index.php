48100c9fdfefe2432e1436682d775ff7786a304f
Adrian <roselmartinezadrian@gmail.com>
2026-09-24 18:00:41 +0200

    Resolucion de ejercicio 34

--- UT1/ejercicios/html_php/ejercicio33/index.php
+++ UT1/ejercicios/html_php/ejercicio33/index.php
@@ -2,23 +2,27 @@
 
 require_once __DIR__ . '/funciones.php';
 require_once __DIR__ . '/datos.php';
+date_default_timezone_set('Europe/Madrid');
 
 $genero = $_GET['genero'] ?? 'todos';
 $genero = strtolower(trim($genero));
 
+//guardo el valor del filtrado en una variable
 $respuesta = filtrarGenero($catalogo, $genero);
 
 sort($respuesta);
 
+echo '<h1>Ejercicio integrado</h1><br>';
+
 echo 'Resultados: ' . count($respuesta) . '<br><br>';
+
+$hoy = new DateTimeImmutable();
+$fechaRevision = $hoy->modify('+30 days');
+
+echo 'Fecha de revisión: ' . $fechaRevision->format('d/m/Y H:i');
+
+//Llamo a la funcion que imprime los titulos del catalogo de libros
+mostrarTitulos($catalogo);
 
 ?>
 
-<ul>
-    <?= htmlspecialchars('Libros: ') ?>
-    <?php foreach($catalogo as $libros): ?>
-    <li>
-        <?= htmlspecialchars($libros['titulo']) ?>
-    </li>
-    <?php endforeach; ?>
-</ul>
