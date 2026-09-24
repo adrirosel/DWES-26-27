c38920c6d7fd8343d3d46bdab8c52cf8f4acc4f4
adrirosel <roselmartinezadrian@gmail.com>
2026-09-24 11:06:24 +0200

    Continuacion ejercicio 34

--- UT1/ejercicios/html_php/ejercicio33/index.php
+++ UT1/ejercicios/html_php/ejercicio33/index.php
@@ -1,12 +1,24 @@
 <?php
 
-require_once 'C:\DWES 26-27\UT1\ejercicios\html_php\ejercicio33|datos.php';
-require_once __DIR__ . './funciones.php';
+require_once __DIR__ . '/funciones.php';
+require_once __DIR__ . '/datos.php';
 
 $genero = $_GET['genero'] ?? 'todos';
 $genero = strtolower(trim($genero));
 
 $respuesta = filtrarGenero($catalogo, $genero);
 
-echo $catalogo[0]['titulo'];
+sort($respuesta);
 
+echo 'Resultados: ' . count($respuesta) . '<br><br>';
+
+?>
+
+<ul>
+    <?= htmlspecialchars('Libros: ') ?>
+    <?php foreach($catalogo as $libros): ?>
+    <li>
+        <?= htmlspecialchars($libros['titulo']) ?>
+    </li>
+    <?php endforeach; ?>
+</ul>
